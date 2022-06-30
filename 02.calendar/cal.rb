2e2337a72af980731511339193676baa7d61edc9
asya81 <asya81jp@gmail.com>
2022-06-30 12:16:27 +0900

    refs #3 カレンダークラスを追加
    メソッド間で共通のインスタンス変数を使いたいため、カレンダークラスを追加
    ・カレンダークラスのインスタンスからメソッド呼び出しするよう修正
    ・初期化メソッドで今日の年、月を取得
    ・メソッド内の処理をインスタンス変数を利用するよう修正
    ・クラスに合わせてメソッド名を変更（generate_calendar -> generate）
    ・インスタンス変数追加によって不要になった仮引数dayを削除
    ・現在の年月以外であっても日付が反転してしまう不具合を修正（条件見直し）
    ・generateメソッド内の条件文を統一するために変数current_dateを追加
    ・ヒアドキュメントのインデントを無視するよう修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,83 +1,91 @@
 #!/usr/bin/env ruby
-require 'date'
-require 'optparse'
-# 年月の先頭のスペースの数
-HEADER_MARGIN_SPACES = 6
-# 1日分の半角スペースの数
-ONE_DAY_SPACES = 3
-MONTH_LABEL = "月"
-DAYS = "日 月 火 水 木 金 土"
-CHARACTER_COLOR_BLACK = "\e[30m"
-BACKGROUND_COLOR_WHITE = "\e[47m"
-RESET_CODE = "\e[0m"
+class Calendar
+  require 'date'
+  require 'optparse'
+  # 年月の先頭のスペースの数
+  HEADER_MARGIN_SPACES = 6
+  # 1日分の半角スペースの数
+  ONE_DAY_SPACES = 3
+  MONTH_LABEL = "月"
+  DAYS = "日 月 火 水 木 金 土"
+  CHARACTER_COLOR_BLACK = "\e[30m"
+  BACKGROUND_COLOR_WHITE = "\e[47m"
+  RESET_CODE = "\e[0m"
 
-def initialize_options(error: "")
-  begin
-    # コマンドラインから受け取った年月
-    options = ARGV.getopts('y:', 'm:')
-  rescue OptionParser::InvalidOption
-    error << "オプションには、 y（年）または m（月）のみ指定できます。"
-  rescue OptionParser::MissingArgument
-    error << "オプションの値を設定してください。"
-  ensure
-    return options, error
+  def initialize
+    @today = Date.today
+    @current_year, @current_month = @today.year, @today.month
+  end
+
+  def initialize_options(error: "")
+    begin
+      # コマンドラインから受け取った年月
+      options = ARGV.getopts('y:', 'm:')
+    rescue OptionParser::InvalidOption
+      error << "オプションには、 y（年）または m（月）のみ指定できます。"
+    rescue OptionParser::MissingArgument
+      error << "オプションの値を設定してください。"
+    ensure
+      return options, error
+    end
+  end
+
+  def initialize_parameters(input_year:, input_month:, errors: [])
+    # コマンドラインからの指定がない場合、現在の年月を表示
+    if input_year.nil?
+      display_year = @current_year
+    elsif input_year.to_i <= 0
+      errors << "y オプションには、1〜9999の整数を指定してください。"
+    else
+      display_year = input_year.to_i
+    end
+    if input_month.nil?
+      display_month = @current_month
+    elsif !(1..12).cover?(input_month.to_i)
+      errors << "m オプションには、1〜12の整数を指定してください。"
+    else
+      display_month = input_month.to_i
+    end
+    return display_year, display_month, errors
+  end
+
+  def generate(year:, month:)
+    # 年月の先頭の余白
+    header_spaces = "\s" * HEADER_MARGIN_SPACES
+    # 第1週の先頭の余白
+    first_week_spaces = "\s" * ONE_DAY_SPACES * Date.new(year, month, 1).wday
+    # 表示月の最終日
+    last_day = Date.new(year, month, -1).day
+    # 表示する日付
+    dates = ""
+    (1..last_day).each do |date|
+      current_date = Date.new(year, month, date)
+      dates << "#{CHARACTER_COLOR_BLACK}#{BACKGROUND_COLOR_WHITE}" if current_date == @today
+      dates << sprintf("%2d", date)
+      dates << RESET_CODE if current_date == @today
+      dates << "\s"
+      dates << "\r\n" if current_date.saturday?
+    end
+    <<~EOF
+    #{header_spaces}#{month}#{MONTH_LABEL}\s#{year}
+    #{DAYS}
+    #{first_week_spaces}#{dates}
+    EOF
   end
 end
 
-def initialize_parameters(input_year:, input_month:, errors: [])
-  # 現在の年月
-  today = Date.today
-  current_year, current_month = today.year, today.month
-  # コマンドラインからの指定がない場合、現在の年月を表示
-  if input_year.nil?
-    display_year = current_year
-  elsif input_year.to_i <= 0
-    errors << "y オプションには、1〜9999の整数を指定してください。"
-  else
-    display_year = input_year.to_i
-  end
-  if input_month.nil?
-    display_month = current_month
-  elsif !(1..12).cover?(input_month.to_i)
-    errors << "m オプションには、1〜12の整数を指定してください。"
-  else
-    display_month = input_month.to_i
-  end
-  return display_year, display_month, today.day.to_i, errors
-end
+calendar = Calendar.new
+options, options_error = calendar.initialize_options
 
-def generate_calendar(year:, month:, day:)
-  # 年月の先頭の余白
-  header_spaces = "\s" * HEADER_MARGIN_SPACES
-  # 第1週の先頭の余白
-  first_week_spaces = "\s" * ONE_DAY_SPACES * Date.new(year, month, 1).wday
-  # 表示月の最終日
-  last_day = Date.new(year, month, -1).day
-  # 表示する日付
-  dates = ""
-  (1..last_day).each do |date|
-    dates << "#{CHARACTER_COLOR_BLACK}#{BACKGROUND_COLOR_WHITE}" if date == day
-    dates << sprintf("%2d", date)
-    dates << RESET_CODE if date == day
-    dates << "\s"
-    dates << "\r\n" if Date.new(year, month, date).saturday?
-  end
-  <<-EOF
-  #{header_spaces}#{month}#{MONTH_LABEL}\s#{year}         
-  #{DAYS}
-  #{first_week_spaces}#{dates}
-  EOF
-end
-
-options, options_error = initialize_options
 if options_error.empty?
-  year, month, day, params_errors = initialize_parameters(input_year: options["y"], input_month: options["m"])
+  year, month, params_errors = calendar.initialize_parameters(input_year: options["y"], input_month: options["m"])
 else
   puts options_error
   return
 end
+
 if params_errors.empty?
-  puts generate_calendar(year: year, month: month, day: day)
+  puts calendar.generate(year: year, month: month)
 else
   puts params_errors
   return
