e68df30ef37b4f48d582c801460b01aa74bcd089
asya81 <asya81jp@gmail.com>
2022-11-28 22:40:07 +0900

    （レビュー指摘修正）テストコード内の変数名およびメソッド名を見直し
    ファイルとディレクトリの両方の意味を含む場合は object を使うよう修正

--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -4,44 +4,44 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class LsTest < Minitest::Test
-  def test_ls_0_files
+  def test_ls_0_objects
     Dir.stub(:glob, %w[]) do
       assert_nil main
     end
   end
 
-  def test_ls_1_file
+  def test_ls_1_object
     assert_equal "1\n", list_objects(%w[1])
   end
 
-  def test_ls_3_files
+  def test_ls_3_objects
     assert_equal "1       2       3\n", list_objects(%w[1 2 3])
   end
 
-  def test_ls_4_files
+  def test_ls_4_objects
     assert_equal "1       3\n2       4\n", list_objects(%w[1 2 3 4])
   end
 
-  def test_ls_5_files
+  def test_ls_5_objects
     assert_equal "1       3       5\n2       4       \n", list_objects(%w[1 2 3 4 5])
   end
 
-  def test_ls_9_files
+  def test_ls_9_objects
     assert_equal "1       4       7\n2       5       8\n3       6       9\n", list_objects(%w[1 2 3 4 5 6 7 8 9])
   end
 
-  def test_ls_10_files
+  def test_ls_10_objects
     assert_equal "1       4       8\n10      5       9\n2       6       \n3       7       \n", list_objects(%w[1 10 2 3 4 5 6 7 8 9])
   end
 
-  def test_ls_long_name_files
-    file_names = %w[01.fizzbuzz 02.calendar 03.rake 04.bowling 05.ls 06.wc 07.bowling_object 08.ls_object 09.wc_object README.md]
+  def test_ls_long_name_objects
+    object_names = %w[01.fizzbuzz 02.calendar 03.rake 04.bowling 05.ls 06.wc 07.bowling_object 08.ls_object 09.wc_object README.md]
     expected = <<~OUTPUT
       01.fizzbuzz             05.ls                   09.wc_object
       02.calendar             06.wc                   README.md
       03.rake                 07.bowling_object       
       04.bowling              08.ls_object            
     OUTPUT
-    assert_equal expected, list_objects(file_names)
+    assert_equal expected, list_objects(object_names)
   end
 end
