587b2765ba7fd963035298f23b2270929f7cf8fd
asya81 <asya81jp@gmail.com>
2025-01-12 17:15:37 +0900

    処理の見直し（リファクタリング）
    - optionがfalseの場合カウント対象外に変更
    - 複数ファイルがある場合のみtotalを計算するように変更
    - total行も含めて出力用の文字列を同じメソッドで作成できるように変更

--- 05.wc/lib/wc.rb
+++ 05.wc/lib/wc.rb
@@ -12,53 +12,57 @@ def wc_output
   opt.parse!(ARGV)
   wc_options.transform_values! { true } if wc_options.values.none?
 
-  counts_by_file = read_files
-  body = format_body(counts_by_file, wc_options)
-  total = format_total(counts_by_file, wc_options) if counts_by_file.size > 1
-  [body, total].join("\n")
+  counts = read_files(wc_options)
+  append_total(counts) if counts.size > 1
+  format_counts(counts)
 end
 
-def read_files
+def read_files(options)
   counts = []
-  l = 0
-  w = 0
-  c = 0
+  row = Hash.new(0)
   ARGF.each do |line|
-    l += 1
-    w += line.split.size
-    c += line.bytesize
+    row[:l] += 1 if options[:lines]
+    row[:w] += line.split.size if options[:words]
+    row[:c] += line.bytesize if options[:bytes]
     next unless ARGF.file.eof?
 
     path = $stdin.tty? ? ARGF.file.path : ''
-    counts << { lines: l, words: w, bytes: c, path: path }
-    l = 0
-    w = 0
-    c = 0
+    row[:path] = path
+    counts << row
+    row = Hash.new(0)
   end
   counts
 end
 
-def format_body(counts, wc_options)
+def append_total(counts)
+  total_row = Hash.new(0)
+  counts[0].each_key do |option|
+    next if option == :path
+
+    total_row[option] = counts.sum { |count| count[option] }
+  end
+  total_row[:path] = 'total'
+  counts << total_row
+end
+
+def format_counts(counts)
   counts.map do |count|
-    row_text = wc_options.map do |key, flag|
-      format_as_tab(count[key]) if flag
+    count.map do |option, value|
+      if option == :path
+        format_path(value)
+      else
+        format_num(value)
+      end
     end.join
-    count[:path].empty? ? row_text : row_text + " #{count[:path]}"
   end
 end
 
-def format_total(counts, wc_options)
-  row_text = wc_options.map do |key, flag|
-    next unless flag
-
-    sum = counts.sum { |count| count[key] }
-    format_as_tab(sum)
-  end.join
-  "#{row_text} total"
+def format_num(num)
+  num.to_s.rjust(8)
 end
 
-def format_as_tab(num)
-  num.to_s.rjust(8)
+def format_path(path)
+  path.empty? ? '' : " #{path}"
 end
 
 puts wc_output if __FILE__ == $PROGRAM_NAME
